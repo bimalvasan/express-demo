5d6975ab3ec22b51285079442b961193c902aa78
Bimal <bimalvasan@gmail.com>
2016-07-04 08:52:42 -0500

    PUT and PATCH

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -30,17 +30,61 @@ var routes = function (Book) {
             });
         });
 
-    bookRouter.route('/:bookId').get(function (req, res) {
-
+    /* Middleware */
+    bookRouter.use('/:bookId', function (req, res, next) {
         Book.findById(req.params.bookId, function (err, book) {
             if (err) {
                 console.log(err);
                 res.status(500).send(err);
             }
-            else
-                res.json(book);
+            else if (book) {
+                req.book = book;
+                next();
+            }
+            else {
+                res.status(404).send('No books!');
+            }
         });
     });
+
+    bookRouter.route('/:bookId')
+        .get(function (req, res) {
+            res.json(req.book);
+        })
+        .put(function (req, res) {
+            req.book.title = req.body.title;
+            req.book.genre = req.body.genre;
+            req.book.author = req.body.author;
+            req.book.read = req.body.read;
+            req.book.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .patch(function (req, res) {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+
+            req.book.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json(req.book);
+                }
+            });
+        });
     return bookRouter;
 };
 
