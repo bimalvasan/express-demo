4abadaf9e2e66decc7ba95050c21ec69abeb1671
Bimal <bimalvasan@gmail.com>
2016-07-06 02:31:25 -0500

    Included test case

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    gulpMocha = require('gulp-mocha');
 
 gulp.task('default', function () {
     nodemon({
@@ -10,7 +11,12 @@ gulp.task('default', function () {
         },
         ignore: ['./node_modules/**']
     })
-    .on('restart', function(){
+    .on('restart', function () {
         console.log('Restarting...');
     });
+});
+
+gulp.task('test', function () {
+    gulp.src('tests/*.js', { read: false })
+        .pipe(gulpMocha({ reporter: 'nyan' }));
 });
